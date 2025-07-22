e297420707189b0617e6d2614e1466f1615292bc
Bob <bobpitaia@gmail.com>
2025-07-22 15:04:15 +0100

    Adding @for and track

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,21 @@
-import { Component, signal, computed } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, Output, computed, input, output, EventEmitter
+  //signal, computed
+} from '@angular/core';
+//import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+//const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -12,18 +25,56 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // @Input({required: true}) id!: string;
+  //  @Input({required: true}) avatar!: string;
+  //  @Input({required: true}) name!: string;
+  @Input({required: true}) user!:{
+    id: string;
+    avatar: string;
+    name: string;
 
+  } ;
+   @Output() select = new EventEmitter<string>();
+   //select = output<string>();
+
+   @Output() doubleClick = new EventEmitter();
+
+   //SIGNAL VERSION
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+// imagePath = computed(()=>{
+//     return 'assets/users/' + this.avatar();
+// })
+
+  get imagePath(){
+    return 'assets/users/' + this.user.avatar
+   }
+
+  onSelectUserUser() {
+    this.select.emit(this.user.id);
+  }
+
+  onDoubleClickUser() {
+    this.doubleClick.emit(this.user.id);
+  }
+
+}
+
+
+
+
+
+  //selectedUser = signal(DUMMY_USERS[randomIndex]);
+  //imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
   // get imagePath(){
   //   return 'assets/users/' + this.selectedUser.avatar
   // }
+  //onSelectUser(){
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
 
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  //}
 
-  }
-}
 
 
